9be2e77eaecb16a48055b6419e043589309bb9a9
itiya <fragment.vertex@gmail.com>
2017-08-21 23:11:32 +0900

    zatsudan

--- lib/ruboty/handlers/fizzbuzz.rb
+++ lib/ruboty/handlers/fizzbuzz.rb
@@ -3,7 +3,7 @@ require "ruboty/astral_zatsudan/actions/fizzbuzz"
 module Ruboty
   module Handlers
     class Fizzbuzz < Base
-      on /fizzbuzz (?<number>.*?)\z/, name: 'fizzbuzz', description: 'output fizzbuzz result'
+      on /fizzbuzz (?<number>.*?)\z/, name: 'fizzbuzz', description: 'output zatsudan fizzbuzz result'
 
       def fizzbuzz(message)
         Ruboty::AstralZatsudan::Actions::Fizzbuzz.new(message).call
