35e18421884c0e4b0c5edc190c9cc93ff31642d5
itiya <fragment.vertex@gmail.com>
2017-09-05 22:41:05 +0900

    [add] 雑談のデータ形式を変更

--- lib/ruboty/handlers/zatsudan.rb
+++ lib/ruboty/handlers/zatsudan.rb
@@ -1,15 +1,27 @@
-require "ruboty/astral_zatsudan/actions/zatsudan"
-
 module Ruboty
   module Handlers
     class Zatsudan < Base
-      on /生きてる？/, name: 'zatsudan', description: 'zatsudan'
-      on /死んでる？/, name: 'zatsudan', description: 'zatsudan'
-      on /つらい/, name: 'zatsudan', description: 'zatsudan'
+      on /生きてる？/, name: 'alive', description: '生きてるかどうか聞く'
+      on /死んでる？/, name: 'dead', description: '死んでるかどうか聞く'
+      on /つらい/, name: 'tsurai', description: 'つらい時の話'
+      on /(無)|(ニャオス)|(駄目)|(無理)/, name: 'sanpo', description: '散歩サジェスト'
 
-      def zatsudan(message)
-        Ruboty::AstralZatsudan::Actions::Zatsudan.new(message).call
+      def alive(message)
+        message.reply("生きるとはどういうことなのでしょうか……？")
       end
+
+      def dead(message)
+        message.reply("死んでるわけないじゃないですか")
+      end
+
+      def tsurai(message)
+        message.reply("人生，数値が上がったり下がったりするだけですから気楽に行きましょう！")
+      end
+
+      def sanpo(message)
+        message.reply("散歩してみてはどうですか？")
+      end
+
     end
   end
 end
