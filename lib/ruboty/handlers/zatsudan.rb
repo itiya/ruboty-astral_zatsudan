f3c66d633c63f1a468e32cf80719944803cb0fba
sanyaitiya <fragment.vertex@gmail.com>
2017-09-10 00:09:32 +0900

    [add] リプライを返せるようにした こまごまとした文言の整理も追加

--- lib/ruboty/handlers/zatsudan.rb
+++ lib/ruboty/handlers/zatsudan.rb
@@ -1,17 +1,22 @@
 module Ruboty
   module Handlers
     class Zatsudan < Base
+      env :ASTRAL_MASTER, "botのリプライ対象"
+
       on /生きてる？/, name: 'alive', description: '生きてるかどうか聞く'
       on /死んでる？/, name: 'dead', description: '死んでるかどうか聞く'
-      on /つらい/, name: 'tsurai', description: 'つらい時の話'
+      on /(つら|辛)い/, name: 'tsurai', description: 'つらい時の話'
       on /(無)|(ニャオス)|(駄目)|(無理)/, name: 'sanpo', description: '散歩サジェスト'
+      on /(頭が)*ぼーっとしてる/, name: 'sanpo', description: '散歩サジェスト'
+      on /annual report/, name: 'annual_report', description: '定時報告'
+      on /おはよう(ございます)?([!！])?/, name: 'hello', description: 'あさのあいさつ'
 
       def alive(message)
-        message.reply("生きるとはどういうことなのでしょうか……？")
+        message.reply("生きるとはどういうことなんでしょうね……？")
       end
 
       def dead(message)
-        message.reply("死んでるわけないじゃないですか")
+        message.reply("死んでるわけないじゃないですか！")
       end
 
       def tsurai(message)
@@ -19,7 +24,15 @@ module Ruboty
       end
 
       def sanpo(message)
-        message.reply("散歩してみてはどうですか？")
+        message.reply("散歩してみてはどうですか？\n橋のあたりまで行ったり，市場のほうに行ってみたりすると良いですよ！")
+      end
+
+      def hello(message)
+        message.reply("おはようございます！\nやることを書き出して，今日も１日がんばりましょうね！")
+      end
+
+      def annual_report(message)
+        message.reply("@#{ENV['ASTRAL_MASTER']} 1時間経ちましたよ！\nやったことを書き出して，次の1時間でやることを書き出してくださいね！\n辛かったり，頭がぼーっとしてたりしたら，私に相談してくださいね？")
       end
 
     end
